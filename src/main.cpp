7465dc618da011e2b6e28bf4c76f142c044f3d42
kiyotone <kunwarkirtan36@gmal.com>
2023-06-29 19:20:24 +0545

    bugged update

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include "App.h"
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
-    // Create a circle shape with radius = 100
-    sf::CircleShape circle(100.f);
-    // Set circle color to green
-    circle.setFillColor(sf::Color::Green);
+    
+    App *app = new App;
+   
+   
+    app->init();
+    app->update();
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.draw(circle);
-        window.display();
-    }
 }
+
